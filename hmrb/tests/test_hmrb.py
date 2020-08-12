ddb47e8b4310cdb40422562412aeabf63ba48eee
Kristian Boda <kboda01@gmail.com>
2020-08-12 15:16:35 +0100

    test: example rust test

--- hmrb/tests/test_hmrb.py
+++ hmrb/tests/test_hmrb.py
@@ -1,4 +1,5 @@
 import pytest
+from hmrb import rust
 
 
 class TestHammurabi:
@@ -13,6 +14,9 @@ class TestHammurabi:
         assert True
         self._teardown()
 
+    def test_rust_example(self):
+        assert rust.rust_lib.is_prime(12) is 0
+        assert rust.rust_lib.is_prime(13) is 1
 
 if __name__ == "__main__":
     pytest.main(args=[__file__])
