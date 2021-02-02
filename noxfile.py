33dcfd69353bbf3ca861b54a31eb4708b21a28a0
Kristian Boda <kristian.boda@babylonhealth.com>
2021-02-02 15:08:12 +0000

    [NLP-1851] Refactor Python package release CI (#12)
    * build: implement release commands
    
    * fix: release-candidate version
    
    * build: add CI workflow
    
    * rm: check_release.py

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 from nox.sessions import Session
 
 nox.options.sessions = "lint", "mypy", "tests"
-locations = "hmrb", "noxfile.py", "docs/conf.py"
+locations = "hmrb", "docs/conf.py"
 
 
 @nox.session(python=["3.7"])
@@ -48,8 +48,8 @@ def safety(session: Session) -> None:
 @nox.session(python=["3.6", "3.7", "3.8", "3.9"])
 def tests(session: Session) -> None:
     session.install("pytest", "pytest-cov")
-    session.run("pip", "install", "-r", "requirements.txt")
-    session.run("pip", "install", "-e", ".")
+    session.install("-r", "requirements.txt")
+    session.install("-e", ".")
     session.run(
         "pytest",
         "tests",
@@ -67,3 +67,25 @@ def changelog(session: Session) -> None:
     args = session.posargs or ["--unreleased"]
     session.install("auto-changelog")
     session.run("auto-changelog", *args)
+
+
+@nox.session(python=["3.7"])
+def docs(session: Session) -> None:
+    args = session.posargs or locations
+    session.install("-r", "doc_requirements.txt")
+    session.install("-r", "requirements.txt")
+    session.install("-e", ".")
+    session.install("sphinx-autobuild")
+    session.install(".")
+    session.run(*args)
+
+
+@nox.session(python=["3.7"])
+def publish(session: Session) -> None:
+    args = session.posargs or ["-r","testpypi"]
+    args += ["dist/*"]
+    session.install("twine", "wheel", "setuptools")
+    session.run("rm", "-rf", "dist", "build", external=True)
+    session.run("python", "setup.py", "--quiet", "sdist", "bdist_wheel")
+    session.run("twine", "check", "--strict", "dist/*")
+    session.run("twine","upload", *args)
