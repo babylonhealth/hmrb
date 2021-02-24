79f7c0e8c0ca2c4cd03552de5b29fc34b93cec5d
Kristian Boda <kristian.boda@babylonhealth.com>
2021-02-24 14:54:44 +0000

    fix: setup.py regex

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def read_requirements(file: str) -> List[str]:
         raise FileNotFoundError(file)
     with open(file) as fd:
         unparsed_requirements = fd.read()
-        return re.findall(r"[\w-]+==[\d.]+", unparsed_requirements)
+        return re.findall(r"[\w-]+[=<>]=[\d.,=<>]+", unparsed_requirements)
 
 setup_params = dict(
     name='hmrb',
