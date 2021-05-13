17663a01e4eb953f4ca3f6f3b01168e50adc3fcb
Kristian Boda <kristian.boda@babylonhealth.com>
2021-05-13 09:48:50 +0100

    refactor: spacy2 example

--- examples/spacy2.py
+++ examples/spacy2.py
@@ -1,7 +1,7 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-sentences = 'I love gorillas. Peter loves gorillas. Jane loves Tarzan.'
+sentences = "I love gorillas. Peter loves gorillas. Jane loves Tarzan."
 
 
 def conj_be(subj: str) -> str:
@@ -21,44 +21,41 @@ def gorilla_clb(seq: list, span: slice, data: dict) -> None:
 
 def lover_clb(seq: list, span: slice, data: dict) -> None:
     print(
-        f'{seq[span][-1]["text"]} is a love interest of'
-        f'{seq[span.start]["text"]}.'
+        f"{seq[span][-1].text} is a love interest of "
+        f"{seq[span.start].text}."
     )
 
 
-clbs = {"gorilla people": gorilla_clb, "lover": lover_clb}
+clbs = {"loves_gorilla": gorilla_clb, "loves_someone": lover_clb}
 
 grammar = """
 Law:
-- callback: "gorilla people"
+- callback: "loves_gorilla"
 (
 ((pos: "PROPN") or (pos: "PRON"))
 (lemma: "love")
 (lemma: "gorilla")
 )
 Law:
-- callback: "lover"
+- callback: "loves_someone"
 (
 (pos: "PROPN")
-(text: "loves")
+(lower: "loves")
 (pos: "PROPN")
 )
 """
 
+
 def jsonify_span(span):
-    jsn = []
-    for token in span:
-        jsn.append({
-            'lemma': token.lemma_,
-            'pos': token.pos_,
-            'lower': token.lower_,
-        })
-    return jsn
+    return [
+        {"lemma": token.lemma_, "pos": token.pos_, "lower": token.lower_}
+        for token in span
+    ]
+
 
 from hmrb.core import SpacyCore
-core = SpacyCore(callbacks=clbs,
-                 map_doc=jsonify_span,
-                 sort_length=True)
+
+core = SpacyCore(callbacks=clbs, map_doc=jsonify_span, sort_length=True)
 
 core.load(grammar)
 nlp.add_pipe(core)
