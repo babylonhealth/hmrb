4e04645a3e6d88a53bb2202462c84ca7ef2c28d4
Kristian Boda <kristian.boda@babylonhealth.com>
2021-05-13 09:49:12 +0100

    tests: lint spacy tests

--- tests/test_spacy.py
+++ tests/test_spacy.py
@@ -1,18 +1,18 @@
 import pytest
+
 spacy = pytest.importorskip("spacy")
 
+
 def jsonify_span(span):
-    jsn = []
-    for token in span:
-        jsn.append({
-            'lemma': token.lemma_,
-            'pos': token.pos_,
-            'lower': token.lower_,
-        })
-    return jsn
+    return [
+        {"lemma": token.lemma_, "pos": token.pos_, "lower": token.lower_}
+        for token in span
+    ]
+
 
 def dummy_callback(seq: list, span: slice, data: dict) -> None:
     print("OK")
+
 
 TEXT = "I feel great today."
 TEXT2 = "I love icecream."
@@ -26,22 +26,26 @@ Law:
 )
 """
 
+
 def test_spacyV2(capsys):
     if spacy.__version__ >= "3.0.0":
         pytest.skip(f"Invalid spacy version {spacy.__version__}")
     nlp = spacy.load("en_core_web_sm")
     from hmrb.core import SpacyCore
-    core = SpacyCore(callbacks={"pytest": dummy_callback},
-                 map_doc=jsonify_span,
-                 sort_length=True)
+
+    core = SpacyCore(
+        callbacks={"pytest": dummy_callback},
+        map_doc=jsonify_span,
+        sort_length=True,
+    )
     core.load(GRAMMAR)
     nlp.add_pipe(core)
     nlp(TEXT)
     captured = capsys.readouterr()
-    assert captured[0] == 'OK\n'
+    assert captured[0] == "OK\n"
     nlp(TEXT2)
     captured = capsys.readouterr()
-    assert captured[0] == ''
+    assert captured[0] == ""
 
 
 def test_spacyV3(capsys):
@@ -59,15 +63,15 @@ def test_spacyV3(capsys):
         return dummy_callback(*args, **kwargs)
 
     conf = {}
-    conf['rules']=GRAMMAR
-    conf['callbacks'] = {"pytest": "callbacks.dummy_callback"}
-    conf['map_doc']="augmenters.jsonify_span"
-    conf['sort_length']=True
+    conf["rules"] = GRAMMAR
+    conf["callbacks"] = {"pytest": "callbacks.dummy_callback"}
+    conf["map_doc"] = "augmenters.jsonify_span"
+    conf["sort_length"] = True
 
     nlp.add_pipe("hammurabi", config=conf)
     nlp(TEXT)
     captured = capsys.readouterr()
-    assert captured[0] == 'OK\n'
+    assert captured[0] == "OK\n"
     nlp(TEXT2)
     captured = capsys.readouterr()
-    assert captured[0] == ''
+    assert captured[0] == ""
